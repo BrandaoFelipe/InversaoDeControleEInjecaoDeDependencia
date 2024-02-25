f48670fc173bba025334a989eb138e462b306f22
Felipe Brandao <brandao154@outlook.com>
2024-02-24 21:37:54 -0300

    exercicio de inversao de controle e injecao de dependencia

---
+++ Interface/Entities/CarRental.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Interface.Entities
+{
+    internal class CarRental
+    {
+        public DateTime Start { get; set; }
+        public DateTime Finish { get; set; }
+
+        public Invoice Invoice { get; set; }
+        public Vehicle Vehicle { get; private set; }
+
+        public CarRental() { }
+
+        public CarRental(DateTime start, DateTime finish, Vehicle vehicle)
+        {
+            Start = start;
+            Finish = finish;
+            Vehicle = vehicle;
+            Invoice = null;
+        }
+    }
+}
